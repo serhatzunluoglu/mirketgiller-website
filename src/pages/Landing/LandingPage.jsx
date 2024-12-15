22578f32dad9d08dc98bc4fc86c8d35290134cd0
Furkan Bozok <118224028+FrknB@users.noreply.github.com>
2024-12-15 22:21:46 +0300

    fix: landing hero component position update

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -3,10 +3,12 @@ import style from './styles.module.scss';
 import Hero from '../../components/hero/hero';
 
 function LandingPage() {
-  return <div className={style.landing}>
-    <CollaborationBar />
-    <Hero />
-  </div>;
+  return (
+    <div className={style.landing}>
+      <Hero />
+      <CollaborationBar />
+    </div>
+  );
 }
 
 export default LandingPage;
