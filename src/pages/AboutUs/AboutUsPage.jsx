210936ee58a9142825def5bcc73be6e47aacfb6d
Furkan Bozok <118224028+FrknB@users.noreply.github.com>
2025-01-03 19:35:58 +0300

    feat: added about us component

--- src/pages/AboutUs/AboutUsPage.jsx
+++ src/pages/AboutUs/AboutUsPage.jsx
@@ -1,10 +1,12 @@
 import './styles.module.scss';
 import Team from '../../components/team/team';
 import TimeFlow from '../../components/timeflow/TimeFlow';
+import AboutUs from '../../components/about-us/aboutus';
 
 function AboutUsPage() {
   return (
     <div>
+      <AboutUs />
       <TimeFlow />
       <Team />
     </div>
