b1e5a32a1863df74897ad09f108f12dceb713ab0
Furkan Bozok <118224028+FrknB@users.noreply.github.com>
2025-01-03 21:19:35 +0300

    fix: our team component image cards

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -3,10 +3,17 @@ import coriss from '../../assets/images/img/img.png';
 import glorius from '../../assets/images/img/glorius.png';
 import glorius2 from '../../assets/images/img/glorius2.png';
 import nikolas from '../../assets/images/img/nikolas.png';
+import { useState } from 'react';
 
-const events = () => {
+const team = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [activeButton, setActiveButton] = useState(1);
+  
   return (
-    <div className={`${style.sectionDark} py-[90px] h-auto `}>
+    <div
+      id="teamSection"
+      className={`${style.sectionDark} py-[90px] bg-[#F9FAFB] h-auto `}
+    >
       {/* starting here */}
       <div className="mx-auto max-w-7xl items-center justify-between  px-8 flex flex-col">
         {/* team start */}
@@ -29,20 +36,23 @@ const events = () => {
         >
           <div className="flex gap-4 flex-wrap items-center justify-center">
             <a
-              href="#"
-              className={`${style.buttonDark} px-[28px] py-[12px] rounded-[50px] primary-color border-solid border-[1px] hover:text-white hover:bg-[#d37c26] transition-all`}
+              onClick={() => setActiveButton(1)}
+              href="#teamSection"
+              className={`${activeButton === 1 ? style.active : style.inactive} ${style.buttonDark} px-[28px] py-[12px] rounded-[50px] primary-color border-solid border-[1px] hover:text-white hover:bg-[#d37c26] transition-all`}
             >
               Yönetim Kurulu
             </a>
             <a
-              href="#"
-              className={`${style.buttonDark} px-[28px] py-[12px] rounded-[50px] primary-color border-solid border-[1px] hover:text-white hover:bg-[#d37c26] transition-all`}
+              onClick={() => setActiveButton(2)}
+              href="#teamSection"
+              className={`${activeButton === 2 ? style.active : style.inactive} ${style.buttonDark} px-[28px] py-[12px] rounded-[50px] primary-color border-solid border-[1px] hover:text-white hover:bg-[#d37c26] transition-all`}
             >
               Departman Başkanları
             </a>
             <a
-              href="#"
-              className={`${style.buttonDark} px-[28px] py-[12px] rounded-[50px] primary-color border-solid border-[1px] hover:text-white hover:bg-[#d37c26] transition-all`}
+              onClick={() => setActiveButton(3)}
+              href="#teamSection"
+              className={`${activeButton === 3 ? style.active : style.inactive} ${style.buttonDark} px-[28px] py-[12px] rounded-[50px] primary-color border-solid border-[1px] hover:text-white hover:bg-[#d37c26] transition-all`}
             >
               Genel Ekip Üyeleri
             </a>
@@ -50,76 +60,170 @@ const events = () => {
         </div>
 
         {/* image area */}
-        <div className="w-full flex justify-center xl:justify-between items-center gap-[30px] flex-wrap">
-          <div className="flex items-center justify-center relative ">
-            <img
-              src={coriss}
-              className="w-[270px] h-[330px] object-cover rounded-[8px]"
-            />
-            <div
-              className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
-            >
-              <h3 className="primary-color body-medium-bold">Coriss Ambady</h3>
-              <p className="primary-text-color body-extra-small-text-regular">
-                Web Developer
-              </p>
-            </div>
-          </div>
+        <div className="w-full flex justify-center 3xl:justify-between items-center gap-[30px] flex-wrap">
+          {activeButton === 1 && (
+            <>
+              <div className="flex items-center justify-center relative ">
+                <img
+                  src={coriss}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Coriss Ambady
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Web Developer
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={glorius}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    glorius Cristian
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    App Developer
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={glorius2}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Glorius Cristian
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Ui / Ux Designer
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={nikolas}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Nikolas Brooten
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Sales Manager
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
 
-          <div className="flex items-center justify-center gap-[30px] relative ">
-            <img
-              src={glorius}
-              className="w-[270px] h-[330px] object-cover rounded-[8px]"
-            />
-            <div
-              className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
-            >
-              <h3 className="primary-color body-medium-bold">
-                glorius Cristian
-              </h3>
-              <p className="primary-text-color body-extra-small-text-regular">
-                App Developer
-              </p>
-            </div>
-          </div>
+          {activeButton === 2 && (
+            <>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={glorius2}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Glorius Cristian
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Ui / Ux Designer
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={nikolas}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Nikolas Brooten
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Sales Manager
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
 
-          <div className="flex items-center justify-center gap-[30px] relative ">
-            <img
-              src={glorius2}
-              className="w-[270px] h-[330px] object-cover rounded-[8px]"
-            />
-            <div
-              className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
-            >
-              <h3 className="primary-color body-medium-bold">
-                Glorius Cristian
-              </h3>
-              <p className="primary-text-color body-extra-small-text-regular">
-                Ui / Ux Designer
-              </p>
-            </div>
-          </div>
-
-          <div className="flex items-center justify-center gap-[30px] relative ">
-            <img
-              src={nikolas}
-              className="w-[270px] h-[330px] object-cover rounded-[8px]"
-            />
-            <div
-              className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
-            >
-              <h3 className="primary-color body-medium-bold">
-                Nikolas Brooten
-              </h3>
-              <p className="primary-text-color body-extra-small-text-regular">
-                Sales Manager
-              </p>
-            </div>
-          </div>
+          {activeButton === 3 && (
+            <>
+              <div className="flex items-center justify-center relative ">
+                <img
+                  src={coriss}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Coriss Ambady
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Web Developer
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={glorius}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    glorius Cristian
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    App Developer
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-[30px] relative ">
+                <img
+                  src={glorius2}
+                  className="w-[270px] h-[330px] object-cover rounded-[8px]"
+                />
+                <div
+                  className={`${style.nameBoxDark} p-4 absolute bottom-4 flex flex-col items-center justify-center rounded-[8px] bg-white w-auto px-[56px]`}
+                >
+                  <h3 className="primary-color body-medium-bold">
+                    Glorius Cristian
+                  </h3>
+                  <p className="primary-text-color body-extra-small-text-regular">
+                    Ui / Ux Designer
+                  </p>
+                </div>
+              </div>
+              
+            </>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
-export default events;
+export default team;
