118b6e4f3f3f99899a95b667383ebd05772e9957
Muzaffer Enes <77478332+enesmuzaffer22@users.noreply.github.com>
2025-02-02 13:06:16 +0300

    feat: added-personal-data-information-text

--- src/router/CustomRouter.jsx
+++ src/router/CustomRouter.jsx
@@ -7,6 +7,7 @@ import EventsPage from '../pages/Events/EventsPage.jsx';
 import EventDetailPage from '../pages/EventDetail/EventDetailPage.jsx';
 import TeamPage from '../pages/Team/TeamPage.jsx';
 import SupportedProjectsPage from '../pages/SupportedProjects/SupportedProjectsPage.jsx';
+import KvkkPage from '../pages/Kvkk/KvkkPage.jsx';
 
 export const CustomRouter = [
   { path: '/', element: <LandingPage /> },
@@ -16,6 +17,7 @@ export const CustomRouter = [
   { path: '/etkinliklerimiz', element: <EventsPage /> },
   { path: 'etkinliklerimiz/:slug', element: <EventDetailPage /> },
   { path: '/ekibimiz', element: <TeamPage /> },
+  { path: '/kvkk', element: <KvkkPage /> },
   { path: '/destekledigimiz-projeler', element: <SupportedProjectsPage /> },
   { path: '*', element: <NotFoundPage /> },
 ];
